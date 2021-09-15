c7299ee36add21e4e5b411b077cdcdd9c5778599
alperkarasuer <20437706+alperkarasuer@users.noreply.github.com>
2021-09-15 10:12:51 +0300

    Changed the structure only

--- main.m
+++ main.m
@@ -2,6 +2,8 @@ clear
 close
 clc 
 
+%% Preliminaries and Inputs
+
 % If the data file doesn't exist then
 % run the script, load the resulting data for initial values as a struct
 if(isfile('data.mat'))
@@ -11,133 +13,45 @@ else
     inits = load('data.mat');
 end
 
+% Input trim conditions (leave empty for defaults)
+trimConds.mach = input('Mach Number: ');
+trimConds.alpha = deg2rad(input('Angle of Attack: '));
+trimConds.beta = deg2rad(input('Sideslip Angle: '));
+trimConds.altitude = input('Altitude: ');
 
-Lbw = @(alpha, beta) [cos(beta)*cos(alpha), sin(beta), -cos(beta)*sin(alpha);...
-    -sin(beta)*cos(alpha), cos(beta), sin(alpha)*sin(beta);...
-    sin(alpha), 0, cos(alpha)];
+% Defaults --> M = 0.9, alpha = 5 deg, beta = 5 deg, h = 3000 m
+if isempty(trimConds.mach)
+    trimConds.mach = 0.9;
+end
+if isempty(trimConds.alpha)
+    trimConds.alpha = deg2rad(5);
+end
+if isempty(trimConds.beta)  
+    trimConds.beta = deg2rad(5);
+end
+if isempty(trimConds.altitude)
+    trimConds.altitude = 3000;
+end
 
-Lbe = @(theta,phi) [cos(theta), 0, -sin(theta);...
-    sin(phi)*sin(theta), cos(phi), sin(phi)*cos(theta);...
-    cos(phi)*sin(theta)*cos(phi) -sin(phi), cos(phi)*cos(theta)];
+% Physical constraints
+physConstr.finLim = 30*pi/180;
+physConstr.delim = 30*pi/180;
+physConstr.drlim = 30*pi/180;
+physConstr.dalim = 30*pi/180;
+physConstr.pLim = deg2rad(0.001);
+physConstr.qLim = deg2rad(200);
+physConstr.rLim = deg2rad(200);
+physConstr.phiLim = deg2rad(0.01);
+physConstr.thetaLim = deg2rad(90);
 
-input_trim = [0.9 5 5 3000];
+% Calculate density, temperature and speed of sound at the given altitude
+[trimConds.rho, trimConds.T, trimConds.speedOfSound] = altitudeProp(trimConds.altitude, inits);
 
-states.mach = input_trim(1);
-states.alpha = input_trim(2)*pi/180;
-states.beta = input_trim(3)*pi/180;
-states.height = input_trim(4);
+%% Solve the problem
+% Find the trim condition --> trimValues = [def_dr, def_de, def_da, p, q, r, theta, phi]
+[trimValues, fval] = trim_search(inits, physConstr, trimConds);
 
-constr.finLim = 30*pi/180;
-constr.delim = 30*pi/180;
-constr.drlim = 30*pi/180;
-constr.dalim = 30*pi/180;
-constr.pLim = deg2rad(0.001);
-constr.qLim = deg2rad(200);
-constr.rLim = deg2rad(200);
-constr.phiLim = deg2rad(0.01);
-constr.thetaLim = deg2rad(90);
-
-%%
-
-[altitude.rho, altitude.T, altitude.speedOfSound] = altitudeProp(states.height, inits);
-
-bodyVel = Lbw(states.alpha, states.beta)*[states.mach*altitude.speedOfSound; 0; 0];
-states.u = bodyVel(1);
-states.v = bodyVel(2);
-states.w = bodyVel(3);
-
-%%
-
-Cd = interp1(inits.Machpoints, inits.Cd_data, states.mach);
-
-Cza = interp1(inits.Machpoints, inits.Cza_data, states.mach);
-
-Czq = interp1(inits.Machpoints, inits.Czq_data, states.mach);
-
-Cma = interp1(inits.Machpoints, inits.Cma_data, states.mach);
-
-Cmq = interp1(inits.Machpoints, inits.Cmq_data, states.mach);
-
-Clp = interp1(inits.Machpoints, inits.Clp_data, states.mach);
-
-Czd = interp1(inits.Machpoints, inits.Czd_data, states.mach);
-
-Cmd = interp1(inits.Machpoints, inits.Cmd_data, states.mach);
-
-Cld = interp1(inits.Machpoints, inits.Cld_data, states.mach);
-
-%%
-
-Cx = Cd;
-
-Cy = @(def_dr, r) Cza*states.beta + Czd*def_dr - Czq*r*inits.d/(2*norm(bodyVel));
-
-Cz = @(def_de, q) Cza*states.alpha + Czd*def_de + Czq*q*inits.d/(2*norm(bodyVel));
-
-Cl = @(def_da, p) Cld*def_da + Clp*p*inits.d/(2*norm(bodyVel));
-
-Cm = @(def_de, q) Cma*states.alpha + Cmd*def_de + Cmq*q*inits.d/(2*norm(bodyVel));
-
-Cn = @(def_dr, r) -Cma*states.beta - Cmd*def_dr + Cmq*r*inits.d/(2*norm(bodyVel));
-
-%%
-
-Qd = 0.5*altitudeProp(states.height, inits)*norm(bodyVel)^2;
-
-forces = @(def_dr, def_de, def_da, p, q, r, theta, phi) ...
-    Qd*inits.d*transpose([Cx, Cy(def_dr, r), Cz(def_de, q)]) + inits.mass*Lbe(theta,phi)*transpose([inits.g 0 0]);
-
-moments = @(def_dr, def_de, def_da, p, q, r) Qd*inits.A*inits.d*transpose([Cl(def_da,p) Cm(def_de, q) Cn(def_dr, r)]);
-
-%%
-
-idx = @(expr, index) expr(index); % helper func
-
-omega = @(p, q, r) [0 -r q; r 0 -p; -q p 0];
-
-velDots = @(def_dr, def_de, def_da, p, q, r, theta, phi) -omega(p,q,r)*[states.u; states.v; states.w] + ...
-    (1/inits.mass)*forces(def_dr, def_de, def_da, p, q, r, theta, phi);
-
-angRateDots = @(def_dr, def_de, def_da, p, q, r) inits.I\moments(def_dr, def_de, def_da, p, q, r) - ...
-    inits.I\(cross([p; q; r], inits.I*[p; q; r]));
-
-
-vDot = @(def_dr, def_de, def_da, p, q, r, theta, phi) idx(velDots(def_dr, def_de, def_da, p, q, r, theta, phi), 2);
-wDot =  @(def_dr, def_de, def_da, p, q, r, theta, phi) idx(velDots(def_dr, def_de, def_da, p, q, r, theta, phi), 3);
-pDot = @(def_dr, def_de, def_da, p, q, r) idx(angRateDots(def_dr, def_de, def_da, p, q, r), 1);
-qDot = @(def_dr, def_de, def_da, p, q, r) idx(angRateDots(def_dr, def_de, def_da, p, q, r), 2);
-rDot = @(def_dr, def_de, def_da, p, q, r) idx(angRateDots(def_dr, def_de, def_da, p, q, r), 3);
-phiDot = @(p, q, r, theta, phi) p + (q*sin(phi) + r*cos(phi))*tan(theta);
-%%
-
-
-J = @(x) (vDot(x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8)))^2 ...
-+ (wDot(x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8)))^2 ... 
-+ (pDot(x(1), x(2), x(3), x(4), x(5), x(6)))^2 + (qDot(x(1), x(2), x(3), x(4), x(5), x(6)))^2 ... 
-+ (rDot(x(1), x(2), x(3), x(4), x(5), x(6)))^2 + (phiDot(x(4), x(5), x(6), x(7), x(8)))^2;
-
-% J = @(def_dr, def_de, def_da, p, q, r, theta, phi) (vDot(def_dr, def_de, def_da, p, q, r, theta, phi))^2 ...
-% + (wDot(def_dr, def_de, def_da, p, q, r, theta, phi))^2 ... 
-% + (pDot(def_dr, def_de, def_da, p, q, r))^2 + (qDot(def_dr, def_de, def_da, p, q, r))^2 ... 
-% + (rDot(def_dr, def_de, def_da, p, q, r))^2 + (phiDot(p, q, r, theta, phi))^2;
-
-lb = -[constr.drlim, constr.delim, constr.dalim, constr.pLim, constr.qLim, constr.rLim, constr.thetaLim, constr.phiLim];
-ub = [constr.drlim, constr.delim, constr.dalim, constr.pLim, constr.qLim, constr.rLim, constr.thetaLim, constr.phiLim];
-
-x0 = (lb+ub)/2;
-A = [1, 1, 1, 0, 0, 0, 0, 0;...
-    -1, 1, 1, 0, 0, 0, 0, 0;...
-    -1, -1, 1, 0, 0, 0, 0, 0;...
-    1, -1, 1, 0, 0, 0, 0, 0];
-
-b = 30*pi/180*ones(size(A,1),1);
-
-Aeq = [];
-beq = [];
-
-    
-[x, fval] = fmincon(J, x0, A, b, Aeq, beq, lb, ub)
-%%
+%% Function to find atmospheric properties 
 function [rho, T, speedOfSound] = altitudeProp(h, consts)
     if((h) <= 10000)
         rho = consts.rho0*(1 - 0.00002256*(h))^4.256;
---
+++ trim_search.m
@@ -0,0 +1,96 @@
+function [trimVals, fval] = trim_search(initCond, constr, trimConds)
+    % Wind axis to body axis transformation matrix
+    Lbw = @(alpha, beta) [cos(beta)*cos(alpha), sin(beta), -cos(beta)*sin(alpha);...
+        -sin(beta)*cos(alpha), cos(beta), sin(alpha)*sin(beta);...
+        sin(alpha), 0, cos(alpha)];
+
+    % NED axis to body axis transformation matrix
+    Lbe = @(theta,phi) [cos(theta), 0, -sin(theta);...
+        sin(phi)*sin(theta), cos(phi), sin(phi)*cos(theta);...
+        cos(phi)*sin(theta)*cos(phi) -sin(phi), cos(phi)*cos(theta)];
+    
+    % Find speeds in body axis
+    bodyVel = Lbw(trimConds.alpha, trimConds.beta)*[trimConds.mach*trimConds.speedOfSound; 0; 0];
+    [u, v, w] = deal(bodyVel(1), bodyVel(2), bodyVel(3));
+    
+    % Interpolation of aerodynamic data
+    Cd = interp1(initCond.Machpoints, initCond.Cd_data, trimConds.mach);
+    Cza = interp1(initCond.Machpoints, initCond.Cza_data, trimConds.mach);
+    Czq = interp1(initCond.Machpoints, initCond.Czq_data, trimConds.mach);
+    Cma = interp1(initCond.Machpoints, initCond.Cma_data, trimConds.mach);
+    Cmq = interp1(initCond.Machpoints, initCond.Cmq_data, trimConds.mach);
+    Clp = interp1(initCond.Machpoints, initCond.Clp_data, trimConds.mach);
+    Czd = interp1(initCond.Machpoints, initCond.Czd_data, trimConds.mach);
+    Cmd = interp1(initCond.Machpoints, initCond.Cmd_data, trimConds.mach);
+    Cld = interp1(initCond.Machpoints, initCond.Cld_data, trimConds.mach);
+    
+    % Functions to calculate aerodynamic coefficients
+    Cx = Cd;
+    Cy = @(def_dr, r) Cza*trimConds.beta + Czd*def_dr - Czq*r*initCond.d/(2*norm(bodyVel));
+    Cz = @(def_de, q) Cza*trimConds.alpha + Czd*def_de + Czq*q*initCond.d/(2*norm(bodyVel));
+    Cl = @(def_da, p) Cld*def_da + Clp*p*initCond.d/(2*norm(bodyVel));
+    Cm = @(def_de, q) Cma*trimConds.alpha + Cmd*def_de + Cmq*q*initCond.d/(2*norm(bodyVel));
+    Cn = @(def_dr, r) -Cma*trimConds.beta - Cmd*def_dr + Cmq*r*initCond.d/(2*norm(bodyVel));
+    
+    % Dynamic pressure
+    Qd = 0.5*trimConds.rho*norm(bodyVel)^2;
+
+    % Functions to create vectors for forces and moments
+    forces = @(def_dr, def_de, def_da, p, q, r, theta, phi) ...
+        Qd*initCond.d*transpose([Cx, Cy(def_dr, r), Cz(def_de, q)]) + initCond.mass*Lbe(theta,phi)*transpose([initCond.g 0 0]);
+    moments = @(def_dr, def_de, def_da, p, q, r) Qd*initCond.A*initCond.d*transpose([Cl(def_da,p) Cm(def_de, q) Cn(def_dr, r)]);
+    
+    % Functions to create vectors of body velocities, angular rates and roll angle
+    omega = @(p, q, r) [0 -r q; r 0 -p; -q p 0];
+    
+    velDots = @(def_dr, def_de, def_da, p, q, r, theta, phi) -omega(p,q,r)*[u; v; w] + ...
+        (1/initCond.mass)*forces(def_dr, def_de, def_da, p, q, r, theta, phi);
+
+    angRateDots = @(def_dr, def_de, def_da, p, q, r) initCond.I\moments(def_dr, def_de, def_da, p, q, r) - ...
+        initCond.I\(cross([p; q; r], initCond.I*[p; q; r]));    
+    
+    phiDot = @(p, q, r, theta, phi) p + (q*sin(phi) + r*cos(phi))*tan(theta);    
+    
+    % Helper function to access individual elements of output of anonymous
+    % function
+    idx = @(expr, index) expr(index);
+
+    % Distribute vectors to individual elements
+    vDot = @(def_dr, def_de, def_da, p, q, r, theta, phi) idx(velDots(def_dr, def_de, def_da, p, q, r, theta, phi), 2);
+    wDot =  @(def_dr, def_de, def_da, p, q, r, theta, phi) idx(velDots(def_dr, def_de, def_da, p, q, r, theta, phi), 3);
+    pDot = @(def_dr, def_de, def_da, p, q, r) idx(angRateDots(def_dr, def_de, def_da, p, q, r), 1);
+    qDot = @(def_dr, def_de, def_da, p, q, r) idx(angRateDots(def_dr, def_de, def_da, p, q, r), 2);
+    rDot = @(def_dr, def_de, def_da, p, q, r) idx(angRateDots(def_dr, def_de, def_da, p, q, r), 3);
+
+    % Cost function
+    % x = [def_dr, def_de, def_da, p, q, r, theta, phi]
+    J = @(x) (vDot(x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8)))^2 ...
+    + (wDot(x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8)))^2 ... 
+    + (pDot(x(1), x(2), x(3), x(4), x(5), x(6)))^2 + (qDot(x(1), x(2), x(3), x(4), x(5), x(6)))^2 ... 
+    + (rDot(x(1), x(2), x(3), x(4), x(5), x(6)))^2 + (phiDot(x(4), x(5), x(6), x(7), x(8)))^2;
+
+    % Upper and lower bounds of deflections
+    lb = -[constr.drlim, constr.delim, constr.dalim, constr.pLim, constr.qLim, constr.rLim, constr.thetaLim, constr.phiLim];
+    ub = [constr.drlim, constr.delim, constr.dalim, constr.pLim, constr.qLim, constr.rLim, constr.thetaLim, constr.phiLim];
+    
+    % Initialize optimization from the mean of upper and lower boundaries
+    x0 = (lb+ub)/2;
+    
+    % Linear inequality constraints
+    A = [1, 1, 1, 0, 0, 0, 0, 0;...
+        -1, 1, 1, 0, 0, 0, 0, 0;...
+        -1, -1, 1, 0, 0, 0, 0, 0;...
+        1, -1, 1, 0, 0, 0, 0, 0]; % di = x*dr + y*de + z*da relation
+
+    b = 30*pi/180*ones(size(A,1),1); % finLim constraints
+
+    % There are no equality constraints and nonlinear constraints
+    Aeq = [];
+    beq = [];
+
+    % Find minimum of the cost function J given the constraints
+    [trimVals, fval] = fmincon(J, x0, A, b, Aeq, beq, lb, ub);
+    
+end
+    
+    
